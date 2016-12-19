7ad7d490322d8c002bc180cca86ca43b8beaa3b2
Ahmed Nuaman <ahmed@ahmednuaman.com>
2016-12-19 15:17:23 +0000

    added vibrate

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,15 @@
 const buttons = document.getElementsByClassName('controls')
 const controls = document.getElementById('controls')
+const vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
 
 for (const button of buttons) {
   button.addEventListener('click', (event) => {
     event.preventDefault()
     event.stopPropagation()
-    
+
     controls.classList.add('hide')
+    vibrate(0)
   })
 }
+
+vibrate([1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500]);
