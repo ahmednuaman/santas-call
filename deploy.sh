0b9111df7f3ee23e7205a2aebbca2c7428548e2e
Ahmed Nuaman <ahmed@ahmednuaman.com>
2016-12-19 15:20:58 +0000

    changed branch

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git init
 git remote add origin git@github.com:ahmednuaman/santas-call.git
 git add .
 git commit -a -m 'deploy, yo'
-git checkout master
-git push origin master -f
+git checkout gh-pages
+git push origin gh-pages -f
 
 echo 'Deployed, yo'
